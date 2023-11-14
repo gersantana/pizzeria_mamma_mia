58ea13b52a67efc39763ae5a0049a5fd5d35fdb4
Gerardo Santana <gerardosantana2013@gmail.com>
2023-11-14 02:30:13 -0300

    :sparkles: ADD: componente MenuPizzas.jsx que contiene la estructura basica de la card, el renderizado de la pizza y el layaut de estas

---
+++ src/components/MenuPIzzas/MenuPizzas.jsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import "./MenuPizzas.css";
+import { PizzaPageContext } from "../../context/MyContext";
+
+const MenuPizzas = () => {
+	const { pizzas } = useContext(PizzaPageContext);
+	return (
+		<>
+			<h2 className="text-center my-8 text-5xl font-bold">Elige tus Pizzas</h2>
+			<div className=" container mx-auto gap-5 grid-pizzas ">
+				{pizzas.map((pizza) => (
+					<div key={pizza.id} className="card" style={{ width: "19rem" }}>
+						<img src={pizza.img} className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h1 className="card-title">{pizza.name.toUpperCase()}</h1>
+						</div>
+						<ul className="list-group list-group-flush">
+							<h3>Ingredientes:</h3>
+							<li className="list-group-item">{pizza.ingredients[0]}</li>
+							<li className="list-group-item">{pizza.ingredients[1]}</li>
+							<li className="list-group-item">{pizza.ingredients[2]}</li>
+							<li className="list-group-item">{pizza.ingredients[3]}</li>
+						</ul>
+                        <p>{`$ ${pizza.price}`}</p>
+						<div className="card-body">
+							<a href="#" className="card-link">
+								Ver mas
+							</a>
+							<button>Añadir</button>
+						</div>
+					</div>
+				))}
+				;
+			</div>
+		</>
+	);
+};
+
+export default MenuPizzas;
+// container-xl d-flex flex-wrap gap-5 justify-content-center
--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,9 +1,11 @@
 import { Hero } from "../../components/Hero/Hero"
+import MenuPizzas from "../../components/MenuPIzzas/MenuPizzas"
 import "./IndexPage.css"
 const IndexPage = () => {
   return (
     <div className="">
         <Hero />
+        <MenuPizzas />
     </div>
   )
 }
